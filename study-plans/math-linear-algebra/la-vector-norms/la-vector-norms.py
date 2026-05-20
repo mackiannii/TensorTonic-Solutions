21938291d214fef667dd0e9e7f94ec6afd704a54
mackiannii <143109898+mackiannii@users.noreply.github.com>
2026-05-20 14:03:23 -0600

    Add solution for la-vector-norms

---
+++ study-plans/math-linear-algebra/la-vector-norms/la-vector-norms.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def vector_norms(v):
+    """
+    Returns: float64 array of shape (3,) containing [L1, L2, L-inf] norms.
+    """
+    v = np.array(v)
+    man_distance = np.sum(np.abs(v))
+    euclidean_len = np.sqrt(np.sum((v)**2))
+    max_abs_val = np.max(np.abs((v)))
+
+    return [man_distance, euclidean_len, max_abs_val]
+    
